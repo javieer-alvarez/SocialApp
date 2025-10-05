6f3b6ee51483bfa63cb4476a7b76e6591ac72b7f
Javier Alvarez <javieralvarezizaguirre@gmail.com>
2025-10-05 16:20:16 -0400

    Refactored AuthView to more ergonomic and beautiful form

---
+++ Socialcademy/Supporting Views/PrimaryButtonStyle.swift
@@ -0,0 +1,25 @@
+//
+//  PrimaryButtonStyle.swift
+//  Socialcademy
+//
+//  Created by Javier Ignacio Alvarez Izaguirre on 5/10/25.
+//
+
+import SwiftUI
+
+struct PrimaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(.white)
+            .background(Color.accentColor)
+            .cornerRadius(10)
+    }
+}
+
+extension ButtonStyle where Self == PrimaryButtonStyle {
+    static var primary: PrimaryButtonStyle {
+        PrimaryButtonStyle()
+    }
+}
--- Socialcademy/Views/AuthView.swift
+++ Socialcademy/Views/AuthView.swift
@@ -15,15 +15,88 @@ struct AuthView: View {
         if viewModel.isAuthenticated {
             MainTabView()
         } else{
-            Form{
-                TextField("Email", text: $viewModel.email)
-                TextField("Password", text: $viewModel.password)
-                Button("Sign In"){
-                    viewModel.signIn()
+            NavigationStack{
+                SignInForm(viewModel: viewModel.makeSignInViewModel()){
+                    NavigationLink("Create Account"){
+                        CreateAccountView(viewModel: viewModel.makeCreateAccountViewModel())
+                    }
                 }
             }
         }
     }
+}
+
+private extension AuthView {
+    
+    struct CreateAccountView: View {
+        
+        @StateObject var viewModel: AuthViewModel.CreateAccountViewModel
+        @Environment(\.dismiss) private var dismiss
+        
+        var body: some View {
+            Form{
+                TextField("Name", text: $viewModel.name)
+                    .textContentType(.name)
+                    .textInputAutocapitalization(.words)
+                TextField("Email", text: $viewModel.email)
+                    .textContentType(.emailAddress)
+                SecureField("Password", text: $viewModel.password)
+                    .textContentType(.newPassword)
+            } footer: {
+                Button("Create Account", action: {viewModel.submit()})
+                    .buttonStyle(.primary)
+                Button("Sign In", action: dismiss.callAsFunction)
+                    .padding()
+            }
+            .navigationBarHidden(true)
+            .onSubmit({viewModel.submit()})
+        }
+    }
+    
+    struct SignInForm<Footer: View>: View {
+        
+        @StateObject var viewModel: AuthViewModel.SignInViewModel
+        @ViewBuilder let footer: () -> Footer
+        
+        
+        var body: some View{
+            Form{
+                TextField("Email", text: $viewModel.email)
+                    .textContentType(.emailAddress)
+                SecureField("Password", text: $viewModel.password)
+                    .textContentType(.password)
+            } footer: {
+                Button("Sign In", action: {viewModel.submit()})
+                    .buttonStyle(.primary)
+                footer()
+                    .padding()
+            }
+            .onSubmit({viewModel.submit()})
+        }
+    }
+    
+    struct Form<Content: View, Footer: View>: View {
+        
+        @ViewBuilder let content: () -> Content
+        @ViewBuilder let footer: () -> Footer
+        
+        var body: some View{
+            VStack{
+                Text("Social")
+                    .font(.title.bold())
+                content()
+                    .textInputAutocapitalization(.never)
+                    .padding()
+                    .background(Color.secondary.opacity(0.15))
+                    .cornerRadius(10)
+                footer()
+            }
+            .navigationBarHidden(true)
+            .padding()
+        }
+        
+    }
+    
 }
 
 #Preview {
